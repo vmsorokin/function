4c08bb6d62f08d69d54bc972d69a4a2a7caecfc0
Vladimir <sorokin-v-m@yandex.ru>
2024-07-18 23:05:41 +0500

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,28 @@
+call = 0
+def count_call():
+    global call
+    call = call + 1
+def string_info(str):
+    count_call()
+    r = len(str)
+    str1 = str.upper()
+    str2 = str.lower()
+    return r, str1, str2
+def is_contains(string, list_to_search):
+    count_call()
+    flag = True
+    for i in range(len(list_to_search)):
+        if string.upper() == list_to_search[i].upper():
+            flag = True
+        elif string.upper() == list_to_search[i].upper():
+            flag = True
+        elif string.upper() == list_to_search[i].upper():
+            flag = True
+        else: flag = False
+    return flag
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urban']))
+print(is_contains('cycle',['recycling', 'cyclic']))
+print(call)
+
